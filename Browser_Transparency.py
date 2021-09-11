dde493e0ef471a7ad15bc2d41422268fd4ece76b
renyuts <renyuts@visionatics.com.tw>
2021-09-11 11:57:48 +0800

    add ArgumentParser

--- Browser_Transparency.py
+++ Browser_Transparency.py
@@ -4,14 +4,26 @@ import winxpgui
 import win32api
 import subprocess
 import time
+from argparse import ArgumentParser
 
 def main():
-    subprocess.Popen("start chrome", shell=True) ## 開啟chrome網頁
-    time.sleep(5) ## 這邊讓程式等個五秒，才能抓到下面顯示的頁簽內容
-    hwnd = win32gui.FindWindow(None, "新分頁 - Google Chrome")  ## 後面的新分頁 - Google Chrome要填入頁簽顯示的名字喔
+    parser = ArgumentParser()
+    parser.add_argument("--cmd", type=str, default='start chrome', help='command line')
+    parser.add_argument("--time", type=int, default=5, help='waiting time')
+    parser.add_argument("--title", type=str, default='新分頁 - Google Chrome', help='pagination title')
+    parser.add_argument("--transparency", type=int, default=30, help='browser transparency')
+    args = parser.parse_args()
+
+    ## 開啟chrome網頁(執行 "start chrome" 命令)
+    subprocess.Popen(args.cmd, shell=True)
+    ## 這邊讓程式等個五秒，才能抓到下面顯示的頁簽內容
+    time.sleep(args.time) 
+    ## 搜尋標籤為 "新分頁 - Google Chrome" 的網頁, return 視窗 或 None
+    hwnd = win32gui.FindWindow(None, args.title)  
 
     win32gui.SetWindowLong (hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong (hwnd, win32con.GWL_EXSTYLE ) | win32con.WS_EX_LAYERED )
-    winxpgui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0,0,0), 30, win32con.LWA_ALPHA) ## 中間的30是透明度，數字越小越淺，可以自由更改
+    ## 中間的30是透明度，數字越小越淺
+    winxpgui.SetLayeredWindowAttributes(hwnd, win32api.RGB(0,0,0), args.transparency, win32con.LWA_ALPHA)
 
 if __name__ == "__main__":
     main()
